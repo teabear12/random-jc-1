19dff980decf1e9340493a4dfbdb76fd3b14b2c8
teabear12 <118160987+teabear12@users.noreply.github.com>
2022-11-15 12:58:26 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-from pymongo import MongoClient
-import certifi
+@app.route('/')
+def home():
+   return render_template('index.html')
 
-ca = certifi.where()
+@app.route("/mars", methods=["POST"])
+def web_mars_post():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'POST 연결 완료!'})
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.pkdgkh7.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.dbsparta
+@app.route("/mars", methods=["GET"])
+def web_mars_get():
+    return jsonify({'msg': 'GET 연결 완료!'})
 
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
